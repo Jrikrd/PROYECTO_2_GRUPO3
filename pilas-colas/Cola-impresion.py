1369e49b615b2c50f651909ae49105894f1c0567
gustavodomingo <63265014+gustavodomingo@users.noreply.github.com>
2020-05-12 22:32:46 -0600

    V0.3
    Finalizado 
    ya estan corregidos los errores

--- pilas-colas/Cola-impresion.py
+++ pilas-colas/Cola-impresion.py
@@ -1,47 +1,68 @@
-import os
-from collections import deque
-# definir la cola
-cola = deque()
-ciclo = True
+import os #Libreria utilizada para funciones de limpiar pantalla y pausa
+from collections import deque  #Libreria utilizada para la funcion de cola
 
+cola = deque() # definir la cola
+ciclo = True #Ciclo de menu
 while  ciclo: 
-    print('+++++++++++++++')
+    os.system("cls") 
+    print('+++++++++++++++')  #Menu
     print('Desea ?')
     print('1) Agregar un elemento a la cola de impresion')
     print('2) imprimir')
     print('3) salir')
     menu = int(input('ingrese una opcion:  '))
-    if menu >0 and menu <4:
-        if menu ==1:
+    if menu >0 and menu <4: #Validacion de ingreso de opcion correcta
+        if menu ==1: # Agregar un archivo a la cola de impresión
             os.system("cls")
             print ('ingrese la ruta de la carpeta en la que quiere listar sus archivos')
-            ruta = input()
+            ruta = input() # Variable para leer la ruta de la carpeta
             os.system('cls')
             print ('Se encontraron los siguientes archivos en la carpeta actual.')
-            # en listar los nombres de los archivos 
+            # en listar los nombres 44de los archivos 
             files = os.listdir(ruta)
-            print(files)
+            cont = 0
+            for elem in files: #Ciclo para mostrar los archivos que estan en la carpeta
+                cont += 1
+                print([cont], elem)
+            print('[0] Cancelar')     
             print('Ingrese el número de archivo que desea agregar a la cola:')
-            archivo = int(input())
-            archivo -=1
-            elemento = files[archivo]
-            print('La cola de impresión es:')
-            i=0
-            print([i+1] , elemento)
-            cola.append(elemento)
-            print(cola)
+            archivo = int(input()) #Variable para seleccionar el archivo que se quiere agregar a la cola
+            if archivo != 0:
+                elemento = files[archivo - 1]
+                cola.append(elemento) #Agregar un archivo a las cola de impresion
+                print('La cola de impresión es:')
+                cont2=0
+                cont3=0
+                for elem in cola:
+                    cont2 += 1
+                    print([cont2], cola[cont3])
+                    cont3 += 1                   
+            os.system("pause")
         if menu == 2:
             if len(cola) > 0:
+                os.system("cls")
                 # extraer el primer elemento en la cola
                 siguiente_elemento = cola.popleft()
 
-                print(f'se imprimio el archivo: {siguiente_elemento}')
-                print(f'Cola de impresión: {cola}')
+                print(f'se imprimio el archivo: {siguiente_elemento}') 
+                if len(cola) > 0:
+                    print('La cola de impresión es:')
+                    cont2=0
+                    cont3=0
+                    for elem in cola:
+                        cont2 += 1
+                        print([cont2], cola[cont3])
+                        cont3 += 1 
+                else:
+                    print('la cola de impresion esta vacia ') 
+
             else:
+                os.system("cls")
                 print('la cola de impresion esta vacia ')  
+            os.system("pause")    
         if menu == 3:
             ciclo = False 
             os.system('cls')     
     else:
         print('ingrese un valor correcto')
-ciclo = True
+        os.system("pause")
